8a9b3d091b198420ab9ef6c9a48482ac6b1a7232
snk-king <snksnk33322@gmail.com>
2023-09-13 21:27:31 +0530

    added close button

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ function setActiveSection(sectionId) {
     const activeSection = document.getElementById(sectionId);
     activeSection.classList.add('active');
 }
+function closeSection(sectionId) {
+    const currentSection = document.getElementById(sectionId);
+    currentSection.classList.remove('active');
+}
   
