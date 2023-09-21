3e8e5324fda023e4dd2990cfa1a96b6965b7f75d
snk-king <snksnk33322@gmail.com>
2023-09-21 22:01:01 +0530

    reseting again for animation

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function setActiveSection(sectionId) {
-    // Find the section with the given ID and add the 'active' class
     const activeSection = document.getElementById(sectionId);
     activeSection.classList.add('active');
 }
