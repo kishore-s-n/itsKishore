97f5d05d7af351659722b07b0e90104e8975f6d6
snk-king <snksnk33322@gmail.com>
2024-01-09 23:25:51 +0530

    Made the about page how i wanted

--- script.js
+++ script.js
@@ -6,3 +6,69 @@ function closeSection(name) {
     const currentSection = document.querySelector(name);
     currentSection.classList.remove('active');
 }
+function setActiveTab(tab,contentId) {
+
+    const tabLinks = document.querySelectorAll('.tab-link');
+    tabLinks.forEach(link => link.classList.remove('active-tab'));
+
+    tab.classList.add('active-tab');
+
+    const content= document.querySelectorAll('.tab-contents');
+    content.forEach(link => link.classList.remove('active-content')); 
+
+    const activeContent=document.getElementById(contentId); 
+    activeContent.classList.add('active-content')
+}
+document.addEventListener("DOMContentLoaded", function() {
+    const pullableWord = document.getElementById('pullableWord');
+    let isDragging = false;
+  
+    pullableWord.addEventListener('mousedown', startDragging);
+  
+    function startDragging(e) {
+      e.preventDefault();
+      isDragging = true;
+  
+      const initialY = e.clientY;
+  
+      function dragMove(e) {
+        if (!isDragging) return;
+  
+        const deltaY = e.clientY - initialY;
+        const translateY = Math.min(Math.max(0, deltaY), 100);
+  
+        pullableWord.style.transform = `translateY(${translateY}px)`;
+  
+        if (translateY === 100) {
+          onPullReached();
+        }
+      }
+  
+      function dragEnd() {
+        isDragging = false;
+  
+        if (parseInt(pullableWord.style.transform.slice(11)) >= 100) {
+          pullableWord.style.transition = 'transform 0.5s ease-in-out';
+          pullableWord.style.transform = 'translateY(0)';
+          setTimeout(() => {
+            pullableWord.style.transition = '';
+          }, 500);
+        }
+  
+        document.removeEventListener('mousemove', dragMove);
+        document.removeEventListener('mouseup', dragEnd);
+      }
+  
+      document.addEventListener('mousemove', dragMove);
+      document.addEventListener('mouseup', dragEnd);
+    }
+  
+    function onPullReached() {
+        console.log('it works')
+        reveals=document.querySelectorAll('.details, .revealedmyself, .introforreveal');
+        reveals.forEach(reveal =>{
+            reveal.classList.add('revealed');
+        })
+    }
+});
+  
