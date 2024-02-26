a2575d56828f19ac7d3dc38089a6bb9c3f0a009f
snk-king <snksnk33322@gmail.com>
2024-02-26 20:49:04 +0530

    hire me content update

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function setActiveSection(name) {
-    const activeSection = document.querySelector(name);
-    activeSection.classList.add('active');
+  const activeSection = document.querySelector(name);
+  activeSection.classList.add('active');
 }
 
 
 
 function closeSection(name) {
-    const currentSection = document.querySelector(name);
-    currentSection.classList.remove('active');
+  const currentSection = document.querySelector(name);
+  currentSection.classList.remove('active');
 }
 
 
 
-function setActiveTab(tab,contentId) {
+function setActiveTab(tab, contentId) {
 
-    const tabLinks = document.querySelectorAll('.tab-link');
-    tabLinks.forEach(link => link.classList.remove('active-tab'));
+  const tabLinks = document.querySelectorAll('.tab-link');
+  tabLinks.forEach(link => link.classList.remove('active-tab'));
 
-    tab.classList.add('active-tab');
+  tab.classList.add('active-tab');
 
-    const content= document.querySelectorAll('.tab-contents');
-    content.forEach(link => link.classList.remove('active-content')); 
+  const content = document.querySelectorAll('.tab-contents');
+  content.forEach(link => link.classList.remove('active-content'));
 
-    const activeContent=document.getElementById(contentId); 
-    activeContent.classList.add('active-content')
+  const activeContent = document.getElementById(contentId);
+  activeContent.classList.add('active-content')
 }
 
 function setActiveServiceTab(title, content) {
@@ -35,56 +35,76 @@ function setActiveServiceTab(title, content) {
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    const pullableWord = document.getElementById('pullableWord');
-    let isDragging = false;
-  
-    pullableWord.addEventListener('mousedown', startDragging);
-  
-    function startDragging(e) {
-      e.preventDefault();
-      isDragging = true;
-  
-      const initialY = e.clientY;
-  
-      function dragMove(e) {
-        if (!isDragging) return;
-  
-        const deltaY = e.clientY - initialY;
-        const translateY = Math.min(Math.max(0, deltaY), 100);
-  
-        pullableWord.style.transform = `translateY(${translateY}px)`;
-  
-        if (translateY === 100) {
-          onPullReached();
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  const pullableWord = document.getElementById('pullableWord');
+  let isDragging = false;
+
+  pullableWord.addEventListener('mousedown', startDragging);
+
+  function startDragging(e) {
+    e.preventDefault();
+    isDragging = true;
+
+    const initialY = e.clientY;
+
+    function dragMove(e) {
+      if (!isDragging) return;
+
+      const deltaY = e.clientY - initialY;
+      const translateY = Math.min(Math.max(0, deltaY), 100);
+
+      pullableWord.style.transform = `translateY(${translateY}px)`;
+
+      if (translateY === 100) {
+        onPullReached();
       }
-  
-      function dragEnd() {
-        isDragging = false;
-  
-        if (parseInt(pullableWord.style.transform.slice(11)) >= 100) {
-          pullableWord.style.transition = 'transform 0.5s ease-in-out';
-          pullableWord.style.transform = 'translateY(0)';
-          setTimeout(() => {
-            pullableWord.style.transition = '';
-          }, 500);
-        }
-  
-        document.removeEventListener('mousemove', dragMove);
-        document.removeEventListener('mouseup', dragEnd);
+    }
+
+    function dragEnd() {
+      isDragging = false;
+
+      if (parseInt(pullableWord.style.transform.slice(11)) >= 100) {
+        pullableWord.style.transition = 'transform 0.5s ease-in-out';
+        pullableWord.style.transform = 'translateY(0)';
+        setTimeout(() => {
+          pullableWord.style.transition = '';
+        }, 500);
       }
-  
-      document.addEventListener('mousemove', dragMove);
-      document.addEventListener('mouseup', dragEnd);
+
+      document.removeEventListener('mousemove', dragMove);
+      document.removeEventListener('mouseup', dragEnd);
     }
-  
-    function onPullReached() {
-        console.log('it works')
-        reveals=document.querySelectorAll('.details, .revealedmyself, .introforreveal');
-        reveals.forEach(reveal =>{
-            reveal.classList.add('revealed');
-        })
-    }
+
+    document.addEventListener('mousemove', dragMove);
+    document.addEventListener('mouseup', dragEnd);
+  }
+
+  function onPullReached() {
+    console.log('it works')
+    reveals = document.querySelectorAll('.details, .revealedmyself, .introforreveal');
+    reveals.forEach(reveal => {
+      reveal.classList.add('revealed');
+    })
+  }
 });
-  
+
+
+function form_submit() {
+  const form = document.getElementById('form');
+  const formData = new FormData(form);
+  console.log(formData);
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://formspree.io/f/moqgwqlv');
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      form.reset();
+      alert('Thank you for your message. We will get back to you as soon as possible.');
+    } else {
+      alert('There was a problem with your submission. Please try again.');
+    }
+  };
+  xhr.send(formData);
+}
+
